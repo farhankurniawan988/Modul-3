19d0d9de07c6cf394d63fd780d7f5ac3844fd7fe
Ganesha Rahman Gibran <136820158+Gibranrhmn@users.noreply.github.com>
2024-10-14 17:25:14 +0700

    Add files via upload

---
+++ 2211104058_Ganesha Rahman Gibran/Unguided/ug_2/pelajaran.cpp
@@ -0,0 +1,14 @@
+#include "pelajaran.h"
+#include <iostream>
+
+pelajaran create_pelajaran(string namapel, string kodepel) {
+    pelajaran pel;
+    pel.namaMapel = namapel;
+    pel.kodeMapel = kodepel;
+    return pel;
+}
+
+void tampil_pelajaran(pelajaran pel) {
+    cout << "Nama Mata Pelajaran: " << pel.namaMapel << endl;
+    cout << "Kode Mata Pelajaran: " << pel.kodeMapel << endl;
+}
