77fb8d0e2293b8e13880fbb595080cb4632d4c2c
rasunaaa <169062934+rasunaaa@users.noreply.github.com>
2024-10-14 21:12:47 +0700

    Add files via upload

---
+++ 2311104043_muhammad shafiq rasuna/UNGUIDED/task2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "pelajaran.h"
+#include "pelajaran.cpp"
+
+using namespace std;
+
+int main() {
+    string namapel = "Struktur Data";
+    string kodepel = "STD";
+
+    pelajaran pel = create_pelajaran(namapel, kodepel);
+
+    tampil_pelajaran(pel);
+
+    return 0;
+}
